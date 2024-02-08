92c937964ab4c8aabaf93735b2c35bb6f48b522f
aerrata <53637476+aerrata@users.noreply.github.com>
2024-02-08 21:10:50 +0800

    feat: install tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config'
 
+import tailwind from '@astrojs/tailwind'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aerrata.github.io',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 })
