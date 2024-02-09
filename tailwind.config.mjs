8bc1d86899a358985e30cbc8d014e66d4bbd22fa
aerrata <53637476+aerrata@users.noreply.github.com>
2024-02-09 19:54:48 +0800

    feat: change font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+        mono: ['DM Mono', ...defaultTheme.fontFamily.mono],
       },
       typography: {
         DEFAULT: {
