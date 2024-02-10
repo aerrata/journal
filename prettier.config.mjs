a396d6b77d1acad7d10209c6975ad293f69de0d0
aerrata <53637476+aerrata@users.noreply.github.com>
2024-02-10 23:40:38 +0800

    chore: setup prettier plugin for astro and tailwindcss

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 9999,
+}
