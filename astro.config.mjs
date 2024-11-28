e30624f081136b886eef5bb99245c5f3ba218b86
aerrata <53637476+aerrata@users.noreply.github.com>
2024-11-28 16:48:17 +0800

    feat: remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aerrata.github.io',
+  trailingSlash: 'never',
   integrations: [
     tailwind({
       applyBaseStyles: false,
